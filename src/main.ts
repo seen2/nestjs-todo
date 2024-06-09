ee215aa5364c860dba3ad5f912d2efa20a7e0eec
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-06-09 12:52:20 +0000

    env

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,17 @@ async function bootstrap() {
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
+/*
+.env.development.local
+DB_HOST=127.0.0.1
+DB_PORT=3306
+DATABASE_USER=root
+DATABASE_PASSWORD=password
+DATABASE_NAME=task_management
+PORT=3000
+SALT_ROUNDS=10
+JWT_SECRET_KEY=some_text_that_is_very_secret
+SESSION_EXPIRES_IN=24h
+DUPLICATE_USERNAME_CODE=1062
+
+*/
